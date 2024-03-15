fc4ec27a9bb38f5e0aa79a622c32a68b2b064020
shahzor khan <shahzoorkhan321@gmail.com>
2024-03-15 17:47:25 +0500

    pulling

--- conversation_db.py
+++ conversation_db.py
@@ -1,10 +1,76 @@
+from openai import OpenAI
 import streamlit as st
+from dotenv import load_dotenv
+from Data import sqlite_data
+
+import os
+
+load_dotenv()
+
+
+print(sqlite_data)
+
+
+
+
+# using function calling it will process the db data
+def Processing_DB(Data):
+
+    return "This is the analyzed output from OpenAI."
+
+
+
+
+def Data(data):
+   
+    return "This is the analyzed output from OpenAI."
+
+
+def ai(user_input):
+    print(user_input)
+    Processing_DB(Data)
+    
+
+
+
+
 
 def main():
-    st.title("My Streamlit App Hi")
+    st.title("Converstaion with your Database Visually")
     st.write("Welcome to my Streamlit app!")
 
-    # Add your Streamlit components here
+
 
 if __name__ == "__main__":
     main()
+    
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+
+
+st.title("💬 Chatbot")
+
+
+
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    
+    
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    print(prompt)
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    user_input = st.chat_message("user").write(prompt)
+    print(user_input)
+    
+    
